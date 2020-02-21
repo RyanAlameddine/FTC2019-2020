8f950c6afc0c3205526d28426af14c14e85de6b7
Bryan Taniguchi <23taniguchib@flintridgeprep.org>
2020-02-20 16:31:37 -0800

    I did some probably important stuff today. Yay.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PushbotSkystoneAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PushbotSkystoneAuto.java
@@ -84,25 +84,26 @@ public class PushbotSkystoneAuto extends LinearOpMode{
             robot.leftMotor.setTargetPosition(140 + getL());
             robot.rightMotor.setTargetPosition(getR() - 140);
             WaitTillTargetReached(50, true);
-            turnAmount = 140 + 17;
+            turnAmount = 140 + 25;
 
 
         }
         else if(x>130){
-            robot.rightMotor.setTargetPosition(getR() + 140);
-            robot.leftMotor.setTargetPosition(getL() - 140);
+            robot.rightMotor.setTargetPosition(getR() + 153);
+            robot.leftMotor.setTargetPosition(getL() - 153);
             WaitTillTargetReached(50, true);
-            turnAmount = -140 - 17;
+            turnAmount = -153 - 25;
         }
 
-        robot.rightMotor.setTargetPosition(getR() + 1000);
-        robot.leftMotor.setTargetPosition(getL() + 1000);
+        robot.rightMotor.setTargetPosition(getR() + 1254);
+        robot.leftMotor.setTargetPosition(getL() + 1254);
         WaitTillTargetReached(50, true);
 
         robot.rightMotor.setPower(.5);
         robot.leftMotor.setPower(.5);
         robot.rightMotor.setTargetPosition(getR() + 483);
         robot.leftMotor.setTargetPosition(getL() + 483);
+
         robot.rightMotor.setPower(.7);
         robot.leftMotor.setPower(.7);
 
@@ -131,8 +132,8 @@ public class PushbotSkystoneAuto extends LinearOpMode{
 
         WaitTillTargetReached(50, true);
 
-        robot.leftMotor.setTargetPosition(getL() + 888);
-        robot.rightMotor.setTargetPosition(getR() + 888);
+        robot.leftMotor.setTargetPosition(getL() + 523);
+        robot.rightMotor.setTargetPosition(getR() + 523);
 
         WaitTillTargetReached(50, true);
 
@@ -143,17 +144,41 @@ public class PushbotSkystoneAuto extends LinearOpMode{
 
         robot.leftMotor.setPower(1);
         robot.rightMotor.setPower(1);
-        robot.leftMotor.setTargetPosition(getL() + 3253);
-        robot.rightMotor.setTargetPosition(getR() + 3253);
+        robot.leftMotor.setTargetPosition(getL() + 3453);
+        robot.rightMotor.setTargetPosition(getR() + 3453);
 
         WaitTillTargetReached(45, true);
 
         robot.leftMotor.setPower(.7);
         robot.rightMotor.setPower(.7);
-        robot.leftMotor.setTargetPosition(getL() - 692);
-        robot.rightMotor.setTargetPosition(getR() + 692);
+        robot.leftMotor.setTargetPosition(getL() - 752);
+        robot.rightMotor.setTargetPosition(getR() + 752);
 
         WaitTillTargetReached(50, true);
+
+
+        robot.leftMotor .setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        RunAuto(false);
+        /*robot.leftServoB.setPosition(0);
+        robot.rightServoB.setPosition(1);
+        sleep(576);
+
+        robot.leftMotor.setPower(.7);
+        robot.rightMotor.setPower(.7);
+        robot.leftMotor.setTargetPosition(getL() + 1257);
+        robot.rightMotor.setTargetPosition(getR() + 957);
+
+        WaitTillTargetReached(50, true);
+
+        robot.leftServoB.setPosition(1);
+        robot.rightServoB.setPosition(0);
+        sleep(482);
+
+        robot.leftMotor.setTargetPosition(getL() + 2862);
+        robot.rightMotor.setTargetPosition(getR() + 2862);
+
+        WaitTillTargetReached(50, true);*/
 
         robot.Stop();
 
@@ -188,6 +213,68 @@ public class PushbotSkystoneAuto extends LinearOpMode{
     }
     private int getR (){
         return robot.rightMotor.getTargetPosition();
+
+
+    }
+
+    public void RunAuto(boolean red){
+
+        robot.leftMotor .setPower(-.3f);
+        robot.rightMotor.setPower(-.3f);
+        sleep(1426);
+
+        robot.Stop();
+
+        robot.leftServoB.setPosition(0);
+        robot.rightServoB.setPosition(1);
+        sleep(1200);
+
+        robot.leftMotor .setPower(red ?  1f : .6f);
+        robot.rightMotor.setPower(red ? .6f :  1f);
+        sleep(1250);
+
+        robot.leftMotor .setPower(!red ? -1 : 1);
+        robot.rightMotor.setPower(!red ?  1 : -1);
+        sleep(1758);
+
+        robot.leftMotor .setPower(-.9f);
+        robot.rightMotor.setPower(-.9f);
+
+        robot.leftServoB.setPosition(1);
+        robot.rightServoB.setPosition(0);
+        sleep(1300);
+
+        robot.leftMotor.setPower(.6f);
+        robot.rightMotor.setPower(.6f);
+        sleep(200);
+
+        robot.leftMotor.setPower(0);
+        sleep(200);
+        robot.leftMotor.setPower(.6f);
+        sleep(400);
+
+        robot.leftMotor.setPower(.6f);
+        robot.rightMotor.setPower(-.6f);
+        sleep(726);
+
+        robot.leftMotor.setPower(0f);
+        robot.rightMotor.setPower(0f);
+
+        robot.leftServoF.setPosition(1);
+        robot.rightServoF.setPosition(0);
+        sleep(768);
+
+        robot.leftMotor.setPower(-.6f);
+        robot.rightMotor.setPower(-.6f);
+        sleep(462);
+
+        robot.leftMotor.setPower(-.6f);
+        robot.rightMotor.setPower(.6f);
+        sleep(635);
+
+        robot.leftMotor.setPower(.7f);
+        robot.rightMotor.setPower(.7f);
+        sleep(294);
 
 
     }
